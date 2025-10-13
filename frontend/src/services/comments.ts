36fe11fd58c495642ed1f00d6842792666705b97
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2025-10-12 21:57:16 -0300

    feat: add comment service

---
+++ frontend/src/services/comments.ts
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+
+const commentsUrl = '/api/comments'
+
+const getAllComments = () => {
+  return axios.get(commentsUrl).then(response => response.data)
+}
+
+const getCommentById = (id: string) => {
+  return axios.get(`${commentsUrl}/${id}`).then(response => response.data)
+}
+
+const getGameComments = (id: string) => {
+  return axios.get(`${commentsUrl}/game/${id}`).then(response => response.data)
+}
+
+const getUserComments = (id: string) => {
+  return axios.get(`${commentsUrl}/user/${id}`).then(response => response.data)
+}
+
+const deleteComment = (id: string) => {
+  return axios.delete(`${commentsUrl}/${id}`).then(response => response.data)
+}
+
+const postComment = (userId: number, gameId: number, content: number) => {
+  const comment = { 'user': userId, 'game': gameId, 'content': content }
+
+  return axios.post(`${commentsUrl}`, comment)
+}
+
+export default {
+  getAllComments,
+  getCommentById,
+  getGameComments,
+  getUserComments,
+  deleteComment,
+  postComment
+}
