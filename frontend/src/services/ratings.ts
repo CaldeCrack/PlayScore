befbd71188e8ce8fd84aaa018bed17b13f4f1fc4
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2025-10-12 21:14:58 -0300

    feat: add ratings endpoints

---
+++ frontend/src/services/ratings.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+const ratingsUrl = '/api/ratings'
+
+const getAllRatings = () => {
+  return axios.get(ratingsUrl).then(response => response.data)
+}
+
+const getRatingById = (id: string) => {
+  return axios.get(`${ratingsUrl}/${id}`).then(response => response.data)
+}
+
+const getGameRatings = (id: string) => {
+  return axios.get(`${ratingsUrl}/game/${id}`).then(response => response.data)
+}
+
+const getUserRatings = (id: string) => {
+  return axios.get(`${ratingsUrl}/user/${id}`).then(response => response.data)
+}
+
+const deleteRating = (id: string) => {
+  return axios.delete(`${ratingsUrl}/${id}`).then(response => response.data)
+}
+
+const postRating = (gameId: number, score: number) => {
+  const rating = {
+    'game': gameId,
+    'score': score
+  }
+
+  return axios.post(`${ratingsUrl}`, rating)
+}
+
+const updateRating = (id: string, score: number) => {
+  return axios.put(`${ratingsUrl}/${id}`, { score }).then(response => response.data)
+}
+
+export default {
+  getAllRatings,
+  getRatingById,
+  getGameRatings,
+  getUserRatings,
+  deleteRating,
+  postRating,
+  updateRating
+}
