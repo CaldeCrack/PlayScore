884da5af994179f1f15978da668d9904ac5bc161
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2025-11-13 16:56:57 -0300

    feat: add action in game slice

--- frontend/src/stores/gameSlice.tsx
+++ frontend/src/stores/gameSlice.tsx
@@ -3,10 +3,12 @@ import type Game from '../types/Game'
 
 export type GameState = {
   games: Game[]
+  setGames: (_games: Game[]) => void
   addGame: (_game: Game) => void
 }
 
 export const createGameSlice: StateCreator<GameState> = (set) => ({
   games: [],
+  setGames: (games: Game[]) => set({ games: games }),
   addGame: (game: Game) => set((state) => ({ games: state.games.concat(game) }))
 })
